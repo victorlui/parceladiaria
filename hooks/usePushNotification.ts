04b9b6d73b7d89b0dcb72c4b1fddcdb35373cd35
Victor Luiz <victorluizgonzalez@gmail.com>
2026-02-20 11:03:08 -0300

    Atualizações

--- hooks/usePushNotification.ts
+++ hooks/usePushNotification.ts
@@ -1,49 +1,88 @@
 import * as Notification from "expo-notifications";
 import * as Linking from "expo-linking";
-import { Alert } from "react-native";
+import { Alert, AppState } from "react-native";
 import Constants from "expo-constants";
 import { useNotificationsStore } from "@/store/notifications";
+import { useEffect, useRef } from "react";
 
-export async function registerForPushNotificationsAsync() {
-  const { status: existingStatus } = await Notification.getPermissionsAsync();
-  const { setPushToken } = useNotificationsStore.getState();
+export function usePushNotification(options?: { disabled?: boolean }) {
+  const appState = useRef(AppState.currentState);
+  const isAlertShown = useRef(false);
+  const disabled = options?.disabled;
 
-  let finalStatus = existingStatus;
+  useEffect(() => {
+    if (disabled) {
+      return;
+    }
 
-  if (existingStatus !== "granted") {
-    const { status } = await Notification.requestPermissionsAsync();
-    finalStatus = status;
-  }
+    const checkAndRegister = async () => {
+      // Se já estamos mostrando um alerta, não faça nada para evitar loops
+      if (isAlertShown.current) return;
 
-  if (finalStatus !== "granted") {
-    Alert.alert(
-      "As notificações estão desativadas.",
-      "Para receber alertas importantes, ative as notificações nas configurações do aplicativo.",
-      [
-        {
-          text: "OK",
-          onPress: () => Linking.openSettings(),
-        },
-      ],
-      { cancelable: false }
-    );
-    return;
-  }
+      const { setPushToken } = useNotificationsStore.getState();
+      const { status: existingStatus } =
+        await Notification.getPermissionsAsync();
+      let finalStatus = existingStatus;
 
-  const isDev = __DEV__;
+      if (existingStatus !== "granted") {
+        const { status } = await Notification.requestPermissionsAsync();
+        finalStatus = status;
+      }
 
-  const token = (
-    await Notification.getExpoPushTokenAsync(
-      isDev
-        ? undefined
-        : {
-            projectId:
-              Constants.easConfig?.projectId ||
-              Constants.expoConfig?.extra?.eas?.projectId,
-          }
-    )
-  ).data;
-  setPushToken(token);
+      if (finalStatus !== "granted") {
+        isAlertShown.current = true;
+        Alert.alert(
+          "As notificações estão desativadas.",
+          "Para receber alertas importantes, ative as notificações nas configurações do aplicativo.",
+          [
+            {
+              text: "OK",
+              onPress: () => {
+                isAlertShown.current = false;
+                Linking.openSettings();
+              },
+            },
+          ],
+          { cancelable: false },
+        );
+        return;
+      }
 
-  return token;
+      const isDev = __DEV__;
+
+      try {
+        const token = (
+          await Notification.getExpoPushTokenAsync(
+            isDev
+              ? undefined
+              : {
+                  projectId:
+                    Constants.easConfig?.projectId ||
+                    Constants.expoConfig?.extra?.eas?.projectId,
+                },
+          )
+        ).data;
+        setPushToken(token);
+      } catch (error) {
+        console.log("Erro ao pegar token:", error);
+      }
+    };
+
+    checkAndRegister();
+
+    const subscription = AppState.addEventListener("change", (nextAppState) => {
+      if (
+        appState.current.match(/inactive|background/) &&
+        nextAppState === "active"
+      ) {
+        checkAndRegister();
+      }
+
+      appState.current = nextAppState;
+    });
+
+    return () => {
+      subscription.remove();
+    };
+  }, [disabled]);
 }
